1eff9ef7d8598f6747aa0cf2e1a6fd7309babe99
Pablo Suárez <03.pablo.ss@gmail.com>
2024-11-29 11:15:00 +0100

    commit

--- Actividad evaluable primera evaluación/GestionPedidos.js
+++ Actividad evaluable primera evaluación/GestionPedidos.js
@@ -1,5 +1,51 @@
-function mostrarMensaje() {
-    alert("¡Has hecho clic en el botón pedidos!");
+function tabla(){
+   
+    document.getElementById('formulario').innerHTML=`
+<h2>Gestión de Pedidos</h2>
+    <form onsubmit= "agregarPedido(event)"id="formPedido">
+        <label for="numeroPedido">Número de Pedido:</label>
+        <input type="number" id="numeroPedido" required><br>
+        <label for="cliente">Cliente:</label>
+        <input type="text" id="cliente" required><br>
+        <label for="fecha">Fecha:</label>
+        <input type="date" id="fecha" required><br>
+        <button type="submit" >Añadir Pedido</button>
+    </form>`;
 }
 
-document.getElementById("BotonPedidos").addEventListener("click", mostrarMensaje);
+function tablapedidos(){
+    document.getElementById('BotonPedidos').innerHTML=`
+    <h3>Pedidos</h3>
+    <table id="tablaPedidos">
+        <thead>
+            <tr>
+                <th>Número</th>
+                <th>Cliente</th>
+                <th>Fecha</th>
+                <th>Procesado</th>
+                <th>Servido</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody></tbody>
+    </table>`;
+}
+
+document.getElementById("BotonPedidos").addEventListener("click", tabla);
+
+function agregarPedido() {
+
+    
+    const numero = parseInt(document.getElementById("numeroPedido").value);
+    const cliente = document.getElementById("cliente").value;
+    const fecha = document.getElementById("fecha").value;
+
+    if (pedidos.some(p => p.numero === numero)) {
+        alert("El número de pedido ya existe.");
+        return;
+    }
+
+    pedidos.push({ numero, cliente, fecha, procesado: false, servido: false });
+    guardarDatos();
+    actualizarTablas();
+}
