f05dcb4272a24ffca5fea3073a8e96304fd6483a
pabloss98 <03.pablo.ss@gmail.com>
2024-12-04 11:07:30 +0100

    Commit

--- Actividad evaluable primera evaluación/GestionPiezas.js
+++ Actividad evaluable primera evaluación/GestionPiezas.js
@@ -24,3 +24,75 @@ function tablaPiezas(){
         <button type="submit">Añadir Pieza</button>
     </form>`;
 }
+
+function tablaPiezasAñadidas(){
+    document.getElementById('tablaPiezasContenedor').innerHTML=`
+    <h3>Piezas</h3>
+    <table id="tablaPiezasAñadidas">
+        <thead>
+            <tr>
+                <th>Número</th>
+                <th>Pedido</th>
+                <th>Largo</th>
+                <th>Ancho</th>
+                <th>Grosor</th>
+                <th>Color</th>
+                <th>Chapeado por ambas caras</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody></tbody>
+    </table>`;
+}
+
+function agregarPieza(event) {
+    event.preventDefault();
+
+    const numero = parseInt(document.getElementById("numeroPieza").value);
+    const numeroPedido = parseInt(document.getElementById("numeroPedidoPieza").value);
+    const largo = parseFloat(document.getElementById("largo").value);
+    const ancho = parseFloat(document.getElementById("ancho").value);
+    const grosor = parseFloat(document.getElementById("grosor").value);
+    const color = document.getElementById("color").value;
+    const chapeado = document.getElementById("chapeado").value;
+
+    if (piezas.some(p => p.numero === numero)) {
+        alert("El número de pieza ya existe.");
+        return;
+    }
+    
+   /* if (!pedidos.some(p => p.numero === numeroPedido)) {
+        alert("El número de pedido asociado no existe.");
+        return;
+    }
+    */
+    piezas.push({ numero, numeroPedido, largo, ancho, grosor, color, chapeado });
+    guardarDatos();
+    actualizarTablas();
+}
+
+
+function actualizarTablas(){
+    const tablaPiezasAñadidas = document.querySelector("#tablaPiezasAñadidas tbody");
+            tablaPiezasAñadidas.innerHTML = "";
+            piezas.forEach(p => {
+                tablaPiezasAñadidas.innerHTML += `
+                    <tr>
+                        <td>${p.numero}</td>
+                        <td>${p.numeroPedido}</td>
+                        <td>${p.largo}</td>
+                        <td>${p.ancho}</td>
+                        <td>${p.grosor}</td>
+                        <td>${p.color}</td>
+                        <td>${p.chapeado}</td>
+                        <td><button onclick="eliminarPieza(${p.numero})">Eliminar</button></td>
+                    </tr>`;
+            });
+}
+
+function eliminarPieza(numero) {
+    const index = piezas.findIndex(p => p.numero === numero);
+    if (index !== -1) piezas.splice(index, 1);
+    guardarDatos();
+    actualizarTablas();
+}
